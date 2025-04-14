93bbae29c143b301098edd3a88efdc37acdc5d6c
Lacuran <kornel.wrzesien@gmail.com>
2025-04-14 17:11:08 +0200

    Added snek assets and base setting init for snek

--- src/main/base/base_setting_class.py
+++ src/main/base/base_setting_class.py
@@ -8,4 +8,23 @@ class BaseSettingsClass:
         self.cell_size = BaseWindowConfig.CELL_SIZE
         self.cell_number = BaseWindowConfig.CELL_NUMBER
         self.clock = BaseWindowConfig.CLOCK
+
         self.apple = BaseAssetsConfig.APPLE
+
+        self.snek_head_up = BaseAssetsConfig.SNEK_HEAD_UP
+        self.snek_head_down = BaseAssetsConfig.SNEK_HEAD_DOWN
+        self.snek_head_left = BaseAssetsConfig.SNEK_HEAD_LEFT
+        self.snek_head_right = BaseAssetsConfig.SNEK_HEAD_RIGHT
+
+        self.snek_body_bend_left = BaseAssetsConfig.SNEK_BODY_BEND_LEFT
+        self.snek_body_bend_right = BaseAssetsConfig.SNEK_BODY_BEND_RIGHT
+        self.snek_body_till_left = BaseAssetsConfig.SNEK_BODY_TILL_LEFT
+        self.snek_body_till_right = BaseAssetsConfig.SNEK_BODY_TILL_RIGHT
+
+        self.snek_body_horizontal = BaseAssetsConfig.SNEK_BODY_HORIZONTAL
+        self.snek_body_vertical = BaseAssetsConfig.SNEK_BODY_VERTICAL
+
+        self.snek_tail_up = BaseAssetsConfig.SNEK_TAIL_UP
+        self.snek_tail_down = BaseAssetsConfig.SNEK_TAIL_DOWN
+        self.snek_tail_left = BaseAssetsConfig.SNEK_TAIL_LEFT
+        self.snek_tail_right = BaseAssetsConfig.SNEK_TAIL_RIGHT
